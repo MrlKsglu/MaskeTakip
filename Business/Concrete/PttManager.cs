156fb370a18e343322fa5638d4d1209840c0d423
aa <aa@PD-MKOSEOGLU>
2024-02-14 14:29:46 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,39 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService//Bu sınıfta maskeler ptt tarafından dağıtılacak
+    {
+        private IApplicantService _applicantService;
+
+        public PttManager(IApplicantService applicantService) // Constructor new yapıldığında
+        {
+
+            _applicantService = applicantService;
+        
+        }
+
+        public void GiveMask(Person person)
+        {
+           
+
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + "  için maske verildi");
+            }
+
+            else
+            {
+                Console.WriteLine(person.FirstName+"  için maske verilemedi");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,75 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "İzmir" };
+
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+
+            sehirler1[0] = "Adana";
+
+            List<string> yeniSehiler1 = new List<string> { "Ankara 1", "İstanbul 1", "İzmir 1" };
+
+            yeniSehiler1.Add("Adana 1");
+
+            foreach (var sehir in yeniSehiler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            Person person1 = new Person();
+            person1.FirstName = "Meral";
+            person1.LastName = "Köseoğlu";
+            person1.DateOfBirthYear = 1991;
+            person1.NationalIdentity = 34612942472;
+
+
+            Person person2 = new Person();
+            person2.FirstName = "Cihat";
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+
+
+
+
+        }
+
+
+        static void SelamVer(string isim = "isimsiz")
+        {
+            Console.WriteLine("Merhaba Meral " + isim);
+        }
+
+
+
+        static int Topla(int sayi1 = 9, int sayi2 = 8)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam:" + sonuc);
+            return sonuc;
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
